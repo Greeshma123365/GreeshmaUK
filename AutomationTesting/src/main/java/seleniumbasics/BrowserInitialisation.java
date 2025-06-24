a1e2f11593e296865b142ea5e80be529a6cd945c
gushakum <gushakum@gushakum.internal.rmplc.net>
2025-06-24 19:51:17 +0530

    24-6 commit

---
+++ AutomationTesting/src/main/java/seleniumbasics/BrowserInitialisation.java
@@ -0,0 +1,21 @@
+package seleniumbasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserInitialisation {
+	public void browserInitialisation()
+	{
+//	WebDriver driver = new ChromeDriver();	
+//	WebDriver driver = new EdgeDriver();
+		WebDriver driver = new FirefoxDriver();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ BrowserInitialisation obj = new BrowserInitialisation();
+ obj.browserInitialisation();
+	}
+
+}
